fd992cefec703dcc7e89c9aaa3eb0f8b2de33282
mimron <muhammadimron39@gmail.com>
2023-02-27 16:49:13 +0700

    Initial commit

---
+++ src/docs/swaggerDef.js
@@ -0,0 +1,21 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'mitra-sales-api documentation',
+    version,
+    license: {
+      name: 'MIT',
+      url: 'https://github.com/mimron/mitra-sales-api/blob/master/LICENSE',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${config.port}/v1`,
+    },
+  ],
+};
+
+module.exports = swaggerDef;
